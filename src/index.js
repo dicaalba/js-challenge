055d7e2db5f3edb61cecc4c373a51af1b4e6377c
Diana <diana@multiplica.com>
2022-06-07 18:40:00 -0500

    Updates

---
+++ src/index.js
@@ -0,0 +1,77 @@
+const $app = document.getElementById('app');
+const $observe = document.getElementById('observe');
+
+const API = 'https://api.escuelajs.co/api/v1/products';
+const KEY = 'initial';
+const INITIAL = '4';
+const PRODUCTS_PER_PAGE = 10;
+
+const init = function () {
+  intersectionObserver.observe($observe);
+  localStorage.setItem(KEY, INITIAL);
+};
+
+const getData = (api) => {
+  fetch(api)
+    .then((response) => response.json())
+    .then((response) => {
+      let products = response;
+
+      if (products.length === 0) {
+        alert('Todos los productos Obtenidos');
+        destroyed();
+        return;
+      }
+      updateInitialValue(products.slice(-1)[0].id);
+
+      let output = products.map((e) => {
+        return (
+          "<article class='Card'><img src='" +
+          e.images[0] +
+          "'/><h2>" +
+          e.title +
+          ' ' +
+          '<small>$ ' +
+          e.price +
+          '</small></h2></article>'
+        );
+      });
+      let newItem = document.createElement('section');
+      newItem.classList.add('Item');
+      newItem.innerHTML = output;
+      $app.appendChild(newItem);
+    })
+    .catch((error) => console.log(error));
+};
+
+function updateInitialValue(value) {
+  localStorage.setItem(KEY, value);
+}
+
+async function loadData() {
+  getData(API + '?offset=' + localStorage.getItem(KEY) + '&limit=' + PRODUCTS_PER_PAGE);
+}
+
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        loadData();
+      }
+    });
+  },
+  {
+    rootMargin: '0px 0px 100% 0px',
+  }
+);
+
+const destroyed = function () {
+  intersectionObserver.disconnect();
+};
+
+window.onbeforeunload = function () {
+  localStorage.removeItem(KEY);
+  return '';
+};
+
+init();
